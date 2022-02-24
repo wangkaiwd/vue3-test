682d5f79b3cb1e40b194feda44780778bd7685d3
wangkaiwd <1162486176@qq.com>
2022-02-24 17:12:42 +0800

    feat: prepare upgrade vue cli

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,17 @@
-module.exports = {
-};
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          modifyVars: {
+            'primary-color': '#1DA57A',
+            'link-color': '#1DA57A',
+            'border-radius-base': '2px',
+          },
+          javascriptEnabled: true,
+        },
+      },
+    },
+  },
+});
